1b5f66e29832086088a998bcddc34a0f4dc4ec62
Alan Yao <zyao02@syr.edu>
2019-04-03 10:24:17 -0400

    Initial Commit

---
+++ Alan's Dice Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alan's Dice Game
+//
+//  Created by Alan Yao on 2019/4/3.
+//  Copyright © 2019 Alan Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
