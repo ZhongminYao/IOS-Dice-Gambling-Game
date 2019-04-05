77d8f2b90dfb3aed40b85c07e8165013617a2b96
Alan Yao <zyao02@syr.edu>
2019-04-05 00:06:04 -0400

    Setup

---
+++ Alan's Dice Game/AuthorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AuthorViewController.swift
+//  Alan's Dice Game
+//
+//  Created by Alan Yao on 2019/4/4.
+//  Copyright © 2019 Alan Yao. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# IOS-Dice-Gambling-Game
